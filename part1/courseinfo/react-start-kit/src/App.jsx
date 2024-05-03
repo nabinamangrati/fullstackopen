2948dd7657483ae8189673c81d8f0bae001e2de2
nabinamangrati <mangratinabina1@gmail.com>
2024-05-03 16:13:10 +0545

    part1a 1.1and 1.2 exercise complete

---
+++ part1/courseinfo/react-start-kit/src/App.jsx
@@ -0,0 +1,69 @@
+const App = () => {
+  //yo chai part 1a ko, mathi ko chai part1b ko
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  //header component
+
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+  };
+
+  //part, exercises
+  const Part = ({ part, exercises }) => {
+    return (
+      <p>
+        {part} {exercises}
+      </p>
+    );
+  };
+  //content component
+  const Content = ({
+    part1,
+    exercises1,
+    part2,
+    exercises2,
+    part3,
+    exercises3,
+  }) => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+      </div>
+    );
+  };
+
+  //total component
+  const Total = ({ exercises1, exercises2, exercises3 }) => {
+    const totalExercises = exercises1 + exercises2 + exercises3;
+    return <p>Number of exercises {totalExercises}</p>;
+  };
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
