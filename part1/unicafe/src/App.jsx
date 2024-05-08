1c5d40a2a1aa692d5f2853fa7a5b716866b69dcd
nabinamangrati <mangratinabina1@gmail.com>
2024-05-08 10:54:03 +0545

    part1 d 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+import Display from "../Display";
+
 const App = () => {
-  return <div>Hello there</div>;
+  let initialState = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  // save clicks of each button to its own state
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+  let [clicks, setClicks] = useState(initialState);
+  let [clicksTotal, setTotal] = useState(0);
+
+  let increaseByOneGood = () => {
+    let newGood = clicks.good + 1;
+    setClicks({ good: newGood, neutral: clicks.neutral, bad: clicks.bad });
+    setTotal(newGood + clicks.neutral + clicks.bad);
+  };
+
+  let increaseByOneNeutral = () => {
+    let newNeutral = clicks.neutral + 1;
+    setClicks({ good: clicks.good, neutral: newNeutral, bad: clicks.bad });
+    setTotal(clicks.good + newNeutral + clicks.bad);
+  };
+
+  let increaseByOneBad = () => {
+    let newBad = clicks.bad + 1;
+    setClicks({ good: clicks.good, neutral: clicks.neutral, bad: newBad });
+    setTotal(clicks.good + clicks.neutral + newBad);
+  };
+
+  return (
+    <div>
+      {/* {clicks.good} */}
+      <h2>give feedBack</h2>
+      <button onClick={increaseByOneGood}>good</button>
+      {/* {clicks.neutral} */}
+      <button onClick={increaseByOneNeutral}>neutral</button>
+      {/* {clicks.bad} */}
+      <button onClick={increaseByOneBad}>bad</button>
+      <h2>statistics</h2>
+      <div>good: {clicks.good}</div>
+      <div>neutral: {clicks.neutral}</div>
+      <div>bad :{clicks.bad}</div>
+      <div>total clicks: {clicksTotal}</div>
+    </div>
+  );
 };
 
 export default App;
