4149901dc3e0705fd855c9d2cb760127c1390c51
nabinamangrati <mangratinabina1@gmail.com>
2024-05-08 15:31:47 +0545

    part1d 1.10

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,13 +1,23 @@
+import StatisticsLine from "./StatisticsLine";
 const Statistics = ({ clicks, clicksTotal, average, percentage }) => {
   if (clicksTotal > 0) {
     return (
       <div>
-        <div>good: {clicks.good}</div>
+        {/* after refactoring and using statline component */}
+        <StatisticsLine text="good" value={clicks.good} />
+        <StatisticsLine text="neutral" value={clicks.neutral} />
+        <StatisticsLine text="bad" value={clicks.bad} />
+        <StatisticsLine text="total clicks" value={clicksTotal} />
+        <StatisticsLine text="average" value={average} />
+        <StatisticsLine text="percentage" value={`${percentage} %`} />
+
+        {/* before refactoring the statistics  */}
+        {/* <div>good: {clicks.good}</div>
         <div>neutral: {clicks.neutral}</div>
         <div>bad :{clicks.bad}</div>
         <div>total clicks: {clicksTotal}</div>
         <div>average:{average}</div>
-        <div>percentage: {percentage}%</div>
+        <div>percentage: {percentage}%</div> */}
       </div>
     );
   }
