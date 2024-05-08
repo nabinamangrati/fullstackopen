3fdce8f57b7371c2f370f02e9743d1946c1bc6f9
nabinamangrati <mangratinabina1@gmail.com>
2024-05-08 14:41:11 +0545

    part1 d 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Display from "../Display";
 import Statistics from "./Statistics";
+import Button from "./Button";
 
 const App = () => {
   let initialState = {
@@ -39,22 +40,25 @@ const App = () => {
   return (
     <div>
       <h2>give feedBack</h2>
-      <button onClick={increaseByOneGood}>good</button>
+      {/* after refactoring and making the button component */}
+      <Button text="good" onClick={() => increaseByOneGood("good")} />
+      <Button text="neutral" onClick={() => increaseByOneNeutral("neutral")} />
+      <Button text="bad" onClick={() => increaseByOneBad("bad")} />
+      {/* without refactoring the button code is like this and also we dont need to make the button component */}
+      {/* <button onClick={increaseByOneGood}>good</button>
       <button onClick={increaseByOneNeutral}>neutral</button>
-      <button onClick={increaseByOneBad}>bad</button>
+      <button onClick={increaseByOneBad}>bad</button> */}
       <h2>Statistics</h2>
-      {clicksTotal > 0 ? (
-        <div>
-          <Statistics
-            clicks={clicks}
-            clicksTotal={clicksTotal}
-            average={average}
-            percentage={percentage}
-          />
-        </div>
-      ) : (
-        <p>No feedback yet</p>
-      )}
+
+      <div>
+        <Statistics
+          clicks={clicks}
+          clicksTotal={clicksTotal}
+          average={average}
+          percentage={percentage}
+        />
+      </div>
+
       {/* mooving these to statistics component  */}
       {/* <div>good: {clicks.good}</div>
       <div>neutral: {clicks.neutral}</div>
--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,13 +1,16 @@
 const Statistics = ({ clicks, clicksTotal, average, percentage }) => {
-  return (
-    <div>
-      <div>good: {clicks.good}</div>
-      <div>neutral: {clicks.neutral}</div>
-      <div>bad :{clicks.bad}</div>
-      <div>total clicks: {clicksTotal}</div>
-      <div>average:{average}</div>
-      <div>percentage: {percentage}%</div>
-    </div>
-  );
+  if (clicksTotal > 0) {
+    return (
+      <div>
+        <div>good: {clicks.good}</div>
+        <div>neutral: {clicks.neutral}</div>
+        <div>bad :{clicks.bad}</div>
+        <div>total clicks: {clicksTotal}</div>
+        <div>average:{average}</div>
+        <div>percentage: {percentage}%</div>
+      </div>
+    );
+  }
+  return <p>No feedback yet</p>;
 };
 export default Statistics;
