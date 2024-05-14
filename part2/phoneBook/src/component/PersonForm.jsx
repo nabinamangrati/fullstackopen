96ecbfc37203a1a917654269c97200a5356b238d
nabinamangrati <mangratinabina1@gmail.com>
2024-05-14 21:54:35 +0545

    form component

---
+++ part2/phoneBook/src/component/PersonForm.jsx
@@ -0,0 +1,21 @@
+const PersonForm = ({
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+  handleSubmit,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+        <br />
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button onClick={handleSubmit}>add</button>
+      </div>
+    </form>
+  );
+};
+export default PersonForm;
