21538cad6f7f62a5a3fce7b5a567e601fee17a33
nabinamangrati <mangratinabina1@gmail.com>
2024-05-20 14:42:28 +0545

    par3 b3.9

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json());
+app.use(cors());
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
 });
