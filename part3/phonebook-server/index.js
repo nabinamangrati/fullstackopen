0b2ecfd1fc91a3fdb643b213e32ea2de2eeb0d6e
nabinamangrati <mangratinabina1@gmail.com>
2024-05-22 00:27:40 +0545

    mongo inserted in index with necesities

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -2,6 +2,25 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const url = `mongodb+srv://nabina1:mypassword@cluster0.f3ksog1.mongodb.net/phonebookServer?retryWrites=true&w=majority&appName=Cluster0`;
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: Number,
+});
+phonebookSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+const Person = mongoose.model("Person", phonebookSchema);
 
 app.use(express.json());
 app.use(cors());
@@ -14,30 +33,11 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = [];
 app.get("/api/persons/", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 app.get("/info", (request, response) => {
   const count = persons.length;
