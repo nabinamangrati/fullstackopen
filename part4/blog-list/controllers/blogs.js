0acb61664eb849bb7ba18c740a38cf9063340b1c
nabinamangrati <mangratinabina1@gmail.com>
2024-05-26 22:42:28 +0545

    refactored blog list

---
+++ part4/blog-list/controllers/blogs.js
@@ -0,0 +1,16 @@
+const app = require("express").Router();
+const Blog = require("../models/blog");
+app.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+module.exports = app;
