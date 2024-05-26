d761110be08989bdc59dd105ee83fe7c986ca48d
nabinamangrati <mangratinabina1@gmail.com>
2024-05-26 22:00:35 +0545

    refactoring index.js to app.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,39 +1,7 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const { PORT } = require("./utils/config");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-const Blog = mongoose.model("Blog", blogSchema);
+const app = require("./app");
 
-const mongoUrl = process.env.MONGO_URL;
-console.log(`Connecting to MongoDB at ${mongoUrl}`);
-mongoose.connect(mongoUrl);
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`, "logging from index");
 });
