e3da0b193767e9ffa972459fddf4be93e893f6d6
nabinamangrati <mangratinabina1@gmail.com>
2024-05-27 11:44:17 +0545

    4.5 favorite blog like count

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -64,3 +64,76 @@ describe("total likes", () => {
     assert.strictEqual(result, 34);
   });
 });
+
+describe("favorite blog", () => {
+  const blogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f9",
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+      likes: 10,
+      __v: 0,
+    },
+  ];
+
+  test("finds the blog with the most likes", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    const expected = {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    };
+
+    assert.deepStrictEqual(
+      { title: result.title, author: result.author, likes: result.likes },
+      expected
+    );
+  });
+
+  test("when list is empty, returns null", () => {
+    const result = listHelper.favoriteBlog([]);
+    assert.strictEqual(result, null);
+  });
+
+  test("when list has one blog, returns that blog", () => {
+    const listWithOneBlog = [
+      {
+        _id: "5a422aa71b54a676234d17f8",
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+        likes: 5,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.favoriteBlog(listWithOneBlog);
+    const expected = {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      likes: 5,
+    };
+
+    assert.deepStrictEqual(
+      { title: result.title, author: result.author, likes: result.likes },
+      expected
+    );
+  });
+});
