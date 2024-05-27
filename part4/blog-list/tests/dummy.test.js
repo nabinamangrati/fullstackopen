067a3929d95c5bfbbe53c7785004f4981f64537f
nabinamangrati <mangratinabina1@gmail.com>
2024-05-27 11:57:05 +0545

    modified 4.5

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -7,8 +7,8 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
-describe("total likes", () => {
-  const listWithOneBlog = [
+describe("total  and favorite blog", () => {
+  const blogs = [
     {
       _id: "5a422aa71b54a676234d17f8",
       title: "Go To Statement Considered Harmful",
@@ -60,38 +60,9 @@ describe("total likes", () => {
   ];
 
   test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
+    const result = listHelper.totalLikes(blogs);
     assert.strictEqual(result, 34);
   });
-});
-
-describe("favorite blog", () => {
-  const blogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
-      likes: 10,
-      __v: 0,
-    },
-  ];
 
   test("finds the blog with the most likes", () => {
     const result = listHelper.favoriteBlog(blogs);
