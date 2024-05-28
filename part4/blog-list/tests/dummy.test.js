8d62150cdf1e7da7b6b4119bea9ec933dc11013a
nabinamangrati <mangratinabina1@gmail.com>
2024-05-28 14:45:50 +0545

    sync&await rfctrd also blog arry mov to testhelper

--- part4/blog-list/tests/dummy.test.js
+++ part4/blog-list/tests/dummy.test.js
@@ -1,5 +1,10 @@
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
 const assert = require("assert");
 const listHelper = require("../utils/list_helper");
+const Blog = require("../models/blog");
+const helpers = require("./test_helpers");
 
 test("dummy returns one", () => {
   const blogs = [];
@@ -7,68 +12,11 @@ test("dummy returns one", () => {
   const result = listHelper.dummy(blogs);
   assert.strictEqual(result, 1);
 });
-describe("total  and favorite blog", () => {
-  const blogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0,
-    },
-  ];
 
-  test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(blogs);
+describe("total  and favorite blog", () => {
+  test("when list has only one blog, equals the likes of that", async () => {
+    const blogs = await helpers.blogsInDb();
+    const result = listHelper.totalLikes(helpers.blogs);
     assert.strictEqual(result, 41);
   });
 
@@ -123,4 +71,27 @@ describe("total  and favorite blog", () => {
     const result = listHelper.mostLikes(blogs);
     assert.strictEqual(result, "Edsger W. Dijkstra");
   });
+});
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogPromises = helpers.blogs.map((blog) => {
+    const blogObject = new Blog(blog);
+    return blogObject.save();
+  });
+  await Promise.all(blogPromises);
+});
+const api = supertest(app);
+test("blogs are returned as json", async () => {
+  await api
+    .get("/api/blogs")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+});
+test("there is one blog", async () => {
+  const response = await helpers.blogsInDb();
+
+  assert.strictEqual(response.length, helpers.blogs.length);
+});
+afterAll(async () => {
+  await mongoose.connection.close();
 });
