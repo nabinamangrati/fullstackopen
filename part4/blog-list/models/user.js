9cedabde948d31af672667692c85c037e1e2cbaf
nabinamangrati <mangratinabina1@gmail.com>
2024-06-03 17:03:50 +0545

    instlld bcrypt & mde user contrllr with userschema

--- part4/blog-list/models/user.js
+++ part4/blog-list/models/user.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
-const blogSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "Blog",
   },
 });
-blogSchema.set("toJSON", {
+userSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
   },
 });
-const Blog = mongoose.model("Blog", blogSchema);
+const User = mongoose.model("User", userSchema);
 
-module.exports = Blog;
+module.exports = User;
