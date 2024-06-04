145b4e7a4aebee6378d5d675d4b5ccc0027f7b4f
nabinamangrati <mangratinabina1@gmail.com>
2024-06-04 15:35:23 +0545

    users controller requird and request post was done

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-const blogController = require("./controllers/blogs");
+const blogsController = require("./controllers/blogs");
+const usersController = require("./controllers/users");
 const { mongoUrl } = require("./utils/config");
 
 mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/blogs", blogController);
+app.use("/api/blogs", blogsController);
+app.use("/api/users", usersController);
 
 module.exports = app;
