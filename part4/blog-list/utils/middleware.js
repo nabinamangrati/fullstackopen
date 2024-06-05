896e47fb36990acac133c315bc7343e685762552
nabinamangrati <mangratinabina1@gmail.com>
2024-06-05 21:21:41 +0545

    4.20. token extractor made in middleware

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,20 +1,31 @@
+// const jwt = require("jsonwebtoken");
 const errorHandler = (error, request, response, next) => {
-  debugger;
   console.error(error.message);
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "JsonWebTokenError") {
+    return response.status(401).json({ error: "token invalid" });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
   }
-  //  else if (error.name === "JsonWebTokenError") {
-  //   return response.status(401).json({ error: "token invalid" });
-  // } else if (error.name === "TokenExpiredError") {
-  //   return response.status(401).json({
-  //     error: "token expired",
-  //   });
-  // }
 
   next(error);
 };
-module.exports = { errorHandler };
+
+const tokenExtractor = (request, response, next) => {
+  console.log(request.token, "req token");
+  const authorization = request.get("authorization");
+  console.log(authorization, "authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    request.token = authorization.replace("Bearer ", "");
+  }
+
+  next();
+};
+
+module.exports = { errorHandler, tokenExtractor };
