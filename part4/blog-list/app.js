70013af8ae292a3bb9f4b1e4da5ca297bc4cf895
nabinamangrati <mangratinabina1@gmail.com>
2024-06-05 14:27:16 +0545

    login js made and added to to the app.js for func

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsController = require("./controllers/blogs");
 const usersController = require("./controllers/users");
+const loginController = require("./controllers/login");
 const { mongoUrl } = require("./utils/config");
 
 mongoose.connect(mongoUrl);
@@ -12,5 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsController);
 app.use("/api/users", usersController);
+app.use("/api/login", loginController);
 
 module.exports = app;
