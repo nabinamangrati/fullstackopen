bda0484247fc2dce88e49651c1cb74c6da9b044b
nabinamangrati <mangratinabina1@gmail.com>
2024-07-21 12:59:31 +0545

    part 2 all complete

--- part2/data-for-countries/src/App.jsx
+++ part2/data-for-countries/src/App.jsx
@@ -5,15 +5,20 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [countries, setCountries] = useState([]);
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [weather, setWeather] = useState(null);
   const [errorMessage, setErrorMessage] = useState("");
-  // console.log(searchTerm, "searchterm value");
+
+  console.log(weather, "weather");
+  // Access API key from environment variable
+  const api_key = import.meta.env.VITE_SOME_KEY;
+  console.log(api_key, "api_key from app.jsx");
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
           `https://restcountries.com/v3.1/name/${searchTerm}`
         );
-        console.log(response.data, "response");
         setCountries(response.data);
         setSelectedCountry(null);
         setErrorMessage("");
@@ -28,20 +33,36 @@ const App = () => {
     }
   }, [searchTerm]);
 
+  useEffect(() => {
+    const fetchWeather = async (capital) => {
+      try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${api_key}&units=metric`
+        );
+        setWeather(response.data);
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        setErrorMessage("Error fetching weather data. Please try again later.");
+      }
+    };
+
+    if (selectedCountry) {
+      fetchWeather(selectedCountry.capital[0]);
+    }
+  }, [selectedCountry]);
+
   const handleSearchChange = (e) => {
     e.preventDefault();
-    // console.log(e.target.value, "value from searchtchange");
     setSearchTerm(e.target.value);
   };
 
   const handleCountryClick = async (countryName) => {
-    console.log(countryName, "countryname");
-
     try {
       const response = await axios.get(
         `https://restcountries.com/v3.1/name/${countryName}`
       );
       setSelectedCountry(response.data[0]);
+      setWeather(null); // Clear previous weather data
       setErrorMessage("");
     } catch (error) {
       console.error("Error fetching country details:", error);
@@ -50,6 +71,7 @@ const App = () => {
       );
     }
   };
+
   const renderErrorMessage = () => {
     if (errorMessage) {
       return <p>{errorMessage}</p>;
@@ -79,11 +101,11 @@ const App = () => {
       {countries.length <= 10 && countries.length >= 1 && (
         <ul>
           {countries.map((country) => (
-            <li
-              key={country.name.common}
-              onClick={() => handleCountryClick(country.name.common)}
-            >
-              {country.name.common}
+            <li key={country.name.common}>
+              {country.name.common}{" "}
+              <button onClick={() => handleCountryClick(country.name.common)}>
+                Show
+              </button>
             </li>
           ))}
         </ul>
@@ -91,12 +113,25 @@ const App = () => {
       {selectedCountry && (
         <div>
           <h2>{selectedCountry.name.common}</h2>
-          <p>Capital: {selectedCountry.capital}</p>
+          <p>Capital: {selectedCountry.capital[0]}</p>
           <p>Area: {selectedCountry.area} km²</p>
           <p>
             Languages: {Object.values(selectedCountry.languages).join(", ")}
           </p>
           <img src={selectedCountry.flags.png} alt="Flag" />
+          {weather && (
+            <div>
+              <h3>Weather in {selectedCountry.capital[0]}</h3>
+              <p>Temperature: {weather.main.temp} °C</p>
+
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                alt="Weather icon"
+                style={{ width: "100px", height: "100px" }}
+              />
+              <p>Wind : {weather.wind.speed} m/s</p>
+            </div>
+          )}
         </div>
       )}
     </div>
