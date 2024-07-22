f71a2c0dab0a16aaf8c2bd10b09759492e834feb
nabinamangrati <mangratinabina1@gmail.com>
2024-07-22 14:50:39 +0545

    part5 5.17 full code test

--- part5/bloglist-frontend/cypress/e2e/blog_frontend.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_frontend.cy.js
@@ -3,7 +3,7 @@ describe("Blog app", function () {
     cy.visit("http://localhost:5173");
   });
 
-  it("Login form is shown", function () {
+  it("loginform is shown", function () {
     cy.contains("show login").click();
     cy.contains("username");
     cy.contains("password");
