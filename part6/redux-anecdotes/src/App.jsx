569660506dddca599ad874c9176ddbc403ab2dc8
nabinamangrati <mangratinabina1@gmail.com>
2024-07-31 13:52:01 +0545

    6.14 data fetched from backend json server

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,18 +1,20 @@
-import { useSelector } from "react-redux";
-
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import anecdoteService from "./services/anecdotes";
+import { useDispatch } from "react-redux";
+import { createAnecdote } from "./reducers/anecdoteReducer";
+import { useEffect } from "react";
 
 const App = () => {
-  const anecdotes = useSelector((state) => {
-    if (state.filter === "") {
-      return state.anecdotes;
-    }
-    return state.anecdotes.filter((anecdote) =>
-      anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService.getAll().then((response) => {
+      console.log(response, "response");
+      dispatch(createAnecdote(response));
+    });
   });
   return (
     <div>
