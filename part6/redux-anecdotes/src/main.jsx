68bb6bfb59519475bf9288861647872e53cf053f
nabinamangrati <mangratinabina1@gmail.com>
2024-08-11 14:42:49 +0545

    6.24 notification for length>5

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -3,13 +3,16 @@ import store from "./store/store";
 import { Provider } from "react-redux";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NotificationContextProvider } from "./NotificationContext";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <NotificationContextProvider>
+        <App />
+      </NotificationContextProvider>
     </QueryClientProvider>
   </Provider>
 );
