bf855dc09a0c4541898c470c434925d210a73da5
nabinamangrati <mangratinabina1@gmail.com>
2024-08-14 14:57:22 +0545

    7.7 country hook

--- part7/country-hook/src/hooks/useCountry.js
+++ part7/country-hook/src/hooks/useCountry.js
@@ -1,8 +1,23 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
+
 const useCountry = (name) => {
   const [country, setCountry] = useState(null);
 
-  useEffect(() => {});
+  useEffect(() => {
+    if (name) {
+      let countriesAxios = axios.get(
+        ` https://studies.cs.helsinki.fi/restcountries/api/name/${name} `
+      );
+
+      countriesAxios
+        .then((result) => {
+          console.log(result.data);
+          setCountry({ data: result.data, found: true });
+        })
+        .catch(() => setCountry({ found: false }));
+    }
+  }, [name]);
 
   return country;
 };
