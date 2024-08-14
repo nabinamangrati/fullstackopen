1141ab518c2ba2ed95af391cfa0b71f73ad0fc44
nabinamangrati <mangratinabina1@gmail.com>
2024-08-14 14:31:56 +0545

    initialization of 7.7 with separate hook folder copied from app.jsx

---
+++ part7/country-hook/src/hooks/useCountry.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {});
+
+  return country;
+};
+export default useCountry;
